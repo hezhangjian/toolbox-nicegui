090993c0b6baa34a1170f07a0d62a1c6c118f99d
Zhangjian He <hezhangjian97@gmail.com>
2025-04-28 22:17:11 +0800

    feat: init commit
    Signed-off-by: Zhangjian He <hezhangjian97@gmail.com>

---
+++ main.py
@@ -0,0 +1,11 @@
+from nicegui import ui
+
+if __name__ == '__main__':
+    ui.run(
+        title='ChuQin ToolBox',
+        favicon='🤖',
+        dark=True,
+        reload=False,
+        show=True,
+        port=8080
+    )

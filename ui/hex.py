06073ab327d62922db29f3470d31e798fe4bf730
Zhangjian He <hezhangjian97@gmail.com>
2025-05-14 16:00:23 +0800

    feat: implemen home page
    Signed-off-by: Zhangjian He <hezhangjian97@gmail.com>

---
+++ ui/hex.py
@@ -0,0 +1,4 @@
+from nicegui import ui
+
+def render_hex() -> None:
+    ui.label('HEX').classes('text-2xl font-bold mb-4')
---
+++ ui/time.py
@@ -0,0 +1,5 @@
+from nicegui import ui
+
+
+def render_time():
+    ui.label('Time').classes('text-2xl font-bold mb-4')
